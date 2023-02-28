3706b83a9668dbd06b9b6e6185d786c43d502772
kevin <kevin28042001@gmail.com>
2023-03-01 00:37:21 +0100

    SUERTE PAPA

--- src/Electrodomesticos/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"awakelab.electrodomestico","l":"Electrodomestico"},{"p":"awakelab.electrodomestico","l":"Lavadora"},{"p":"awakelab.ejecutable","l":"Main"},{"p":"awakelab.electrodomestico","l":"Televisor"}]
